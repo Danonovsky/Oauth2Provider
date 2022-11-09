99d6925751128f7e4864608de59f9ac8c810e420
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-11-09 10:17:16 +0100

    Added database, that actually works

--- DAL/IdentityContext.cs
+++ DAL/IdentityContext.cs
@@ -4,4 +4,5 @@ class IdentityContext : DbContext
 {
     public DbSet<User> Users {get;set;} = null!;
     public IdentityContext(DbContextOptions options) : base(options) {}
+    public IdentityContext() : base(){}
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json");
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<IdentityContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
